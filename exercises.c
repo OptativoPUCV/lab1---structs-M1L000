ac8cbe35be673ed6cfa5393e8bd3e4532efafcb6
Your Name <you@example.com>
2024-03-14 21:17:10 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -179,4 +179,8 @@ typedef struct nodo {
   struct nodo *siguiente; // puntero al siguiente nodo
 } Nodo;
 
-Nodo *crearListaEnlazada(int arr[], int size) { return NULL; }
+Nodo *crearListaEnlazada(int arr[], int size) 
+{ 
+  
+  return NULL; 
+}
